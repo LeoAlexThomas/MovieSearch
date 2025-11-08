e92c85fd99a3dc3f757ae00ee54ff8ecc5251409
LeoAlexThomas <leoalex960@gmail.com>
2025-11-08 12:00:54 +0530

    moved provider from app.jsx to main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,19 @@
-import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./Components/Header";
 import Home from "./Pages/Home";
 import MovieDetails from "./Pages/MovieDetails";
-import { MovieFilterProvider } from "./Context/movieContext";
 
 const App = () => {
   return (
     <div className="bg-black-primary h-screen overflow-y-auto">
-      <MovieFilterProvider>
-        <BrowserRouter>
-          <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            {/* Dynamic page routing */}
-            <Route path="/movie/:id" element={<MovieDetails />} />
-          </Routes>
-        </BrowserRouter>
-      </MovieFilterProvider>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* Dynamic page routing */}
+          <Route path="/movie/:id" element={<MovieDetails />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
